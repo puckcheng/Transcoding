333354126c214f1216e660e362be75677587c43a
Puck <puckcheng@gmail.com>
2017-04-20 11:29:33 -0600

    update

--- Vapoursynth/x265d10.bat
+++ Vapoursynth/x265d10.bat
@@ -2,6 +2,13 @@
 
 cd "/d %~dp0"
 
-"C:\Program Files (x86)\VapourSynth\core64\vspipe.exe" --y4m "%~f1"  - | "C:\Program Files (x86)\MeGUI_2715_x86\tools\x265\x64\x265.exe" --y4m --preset slow --frame-threads 8 --crf 18 --deblock -1:-1 -D 10 --merange 44 --aq-strength 0.8 --qcomp 0.65 --output "D:\Media\TEMP\temp.hevc" -
+set str_time_first_bit="%time:~0,1%"  
+if %str_time_first_bit%==" " (  
+    set str_date_time=%date:~0,4%%date:~5,2%%date:~8,2%_0%time:~1,1%%time:~3,2%%time:~6,2%)else (   
+    set str_date_time=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%)  
+set output=%str_date_time%
+
+"C:\Program Files (x86)\VapourSynth\core64\vspipe.exe" --y4m "%~f1"  - | "C:\Program Files (x86)\MeGUI_2715_x86\tools\x265\x64\x265.exe" --y4m --preset slow --frame-threads 8 --crf 20 --deblock 0:0 -D 10 --merange 44 --aq-strength 0.8 --qcomp 0.65 --output "D:\Media\TEMP\%output%.hevc" -
 
 pause
+
