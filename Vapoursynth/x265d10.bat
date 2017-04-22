19a6d58a81eb1b9aa7a6d111891b2156d8261e19
Puck <puckcheng@gmail.com>
2017-04-22 16:33:49 -0600

    update

--- Vapoursynth/x265d10.bat
+++ Vapoursynth/x265d10.bat
@@ -12,7 +12,7 @@ echo src=core.std.SetFrameProp(src,prop="_FieldBased",intval=0) >> "%~dpn1.vpy"
 echo BM3D=mvf.BM3D(src,sigma=[3,3,3],radius1=0) >> "%~dpn1.vpy"
 echo BM3D.set_output() >> "%~dpn1.vpy"
 
-ping 127.0.0.1 -n 2
+ping 127.0.0.1 -n 2 >nul
 SetLocal EnableDelayedExpansion
 
     For /F "Usebackq Delims=" %%j In ("%~dpn1.vpy") Do (
@@ -22,11 +22,13 @@ SetLocal EnableDelayedExpansion
     Del "%~dpn1.vpy" >nul
 
 SetLocal DisableDelayedExpansion
-ping 127.0.0.1 -n 2
+ping 127.0.0.1 -n 2 >nul
 
 "C:\Program Files (x86)\VapourSynth\core64\vspipe.exe" --y4m "PreFlow.vpy"  - | "C:\Program Files (x86)\MeGUI_2715_x86\tools\x265\x64\x265.exe" --y4m --preset slow --frame-threads 8 --crf 20 --deblock 0:0 -D 10 --merange 44 --aq-strength 0.8 --qcomp 0.65 --output "D:\Media\TEMP\%~n1.hevc" -
 
 Echo  
+ping 127.0.0.1 -n 1 >nul
+mshta vbscript:createobject("sapi.spvoice").speak("Job done!")(window.close)
 
 pause
 pause
