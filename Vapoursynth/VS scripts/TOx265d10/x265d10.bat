d67cd5b6bc459aaf5d8e9091c9c7065cfe58c08c
Puck <puckcheng@gmail.com>
2017-05-01 18:07:07 -0600

    update

--- Vapoursynth/VS scripts/TOx265d10/x265d10.bat
+++ Vapoursynth/VS scripts/TOx265d10/x265d10.bat
@@ -12,7 +12,7 @@ echo src=core.std.SetFrameProp(src,prop="_FieldBased",intval=0) >> "%~dpn1.vpy"
 echo BM3D=mvf.BM3D(src,sigma=[3,3,3],radius1=0) >> "%~dpn1.vpy"
 echo BM3D.set_output() >> "%~dpn1.vpy"
 
-Echo 生成文件索引中，请耐心等待。。。
+Echo Building File Index......
 
 "C:\Program Files (x86)\VapourSynth\core64\vspipe.exe" --y4m "%~dpn1.vpy"  - | "C:\Program Files (x86)\MeGUI_2715_x86\tools\x265\x64\x265.exe" --y4m --preset slow --frame-threads 8 --crf 20 --deblock 0:0 -D 10 --merange 44 --aq-strength 0.8 --qcomp 0.65 --output "D:\Media\TEMP\%~n1.hevc" -
 
@@ -20,10 +20,11 @@ Echo 
 ping 127.0.0.1 -n 1 >nul
 mshta vbscript:createobject("sapi.spvoice").speak("Job done!")(window.close)
 
+Echo Press Space to delete TEMP files
 pause
 del "%~dpn1.vpy"
-del "%~dpn1.lwi"
-Echo 删除vpy以及lwi文件成功！
+del "%~dpn1%~x1.lwi"
+Echo done!
 pause
 
 
