f51624953e585958a2cd1040060ee5c2e4952bd9
Puck <puckcheng@gmail.com>
2017-08-19 22:10:07 -0600

    update

--- Convert bat/extract audio.bat
+++ Convert bat/extract audio.bat
@@ -1,5 +1,5 @@
 @ECHO OFF  & CD/D "%~dp1"
 
-"C:\Program Files (x86)\MeGUI_2715_x86\tools\ffmpeg\ffmpeg.exe" -i "%~dpn1%~x1" -acodec copy -vn "%~dpn1.dts"
+"C:\Program Files (x86)\MeGUI_2715_x86\tools\ffmpeg\ffmpeg.exe" -i "%~dpn1%~x1" -acodec copy -vn "%~dpn1.m4a"
 
 pause
